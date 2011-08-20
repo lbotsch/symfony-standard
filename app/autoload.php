f72168cf598cefddf13448d703a4f6e683426efc
Lukas Botsch <lukas.botsch@gmail.com>
2011-08-20 15:53:52 +0200

    Update autoload to use /usr/local/lib/symfony-2

--- app/autoload.php
+++ app/autoload.php
@@ -3,32 +3,35 @@
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
+$vendorDir = '/usr/local/lib/symfony-2/vendor';
+
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
-    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/../vendor/monolog/src',
-    'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    'Symfony'          => array($vendorDir.'/symfony/src', $vendorDir.'/bundles'),
+    'Sensio'           => $vendorDir.'/bundles',
+    'JMS'              => $vendorDir.'/bundles',
+    'Doctrine\\Common' => $vendorDir.'/doctrine-common/lib',
+    'Doctrine\\DBAL'   => $vendorDir.'/doctrine-dbal/lib',
+    'Doctrine'         => $vendorDir.'/doctrine/lib',
+    'Monolog'          => $vendorDir.'/monolog/src',
+    'Assetic'          => $vendorDir.'/assetic/src',
+    'Metadata'         => $vendorDir.'/metadata/src',
 ));
 $loader->registerPrefixes(array(
-    'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
-    'Twig_'            => __DIR__.'/../vendor/twig/lib',
+    'Twig_Extensions_' => $vendorDir.'/twig-extensions/lib',
+    'Twig_'            => $vendorDir.'/twig/lib',
 ));
 
 // intl
 if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+    require_once $vendorDir.'/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
-    $loader->registerPrefixFallbacks(array(__DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs'));
+    $loader->registerPrefixFallbacks(array($vendorDir.'/symfony/src/Symfony/Component/Locale/Resources/stubs'));
 }
 
 $loader->registerNamespaceFallbacks(array(
     __DIR__.'/../src',
+    $vendorDir.'/bundles',
 ));
 $loader->register();
 
@@ -36,10 +39,10 @@ AnnotationRegistry::registerLoader(function($class) use ($loader) {
     $loader->loadClass($class);
     return class_exists($class, false);
 });
-AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+AnnotationRegistry::registerFile($vendorDir.'/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
 // Swiftmailer needs a special autoloader to allow
 // the lazy loading of the init file (which is expensive)
-require_once __DIR__.'/../vendor/swiftmailer/lib/classes/Swift.php';
-Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/lib/swift_init.php');
+require_once $vendorDir.'/swiftmailer/lib/classes/Swift.php';
+Swift::registerAutoload($vendorDir.'/swiftmailer/lib/swift_init.php');
 
